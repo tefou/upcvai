ce5e473975ad8754362159a453528c6769155a3d
TanThien <tanthien.ttt@gmail.com>
2025-02-24 18:14:24 +0700

    updateV1

--- src/app/(main)/billing/success/page.tsx
+++ src/app/(main)/billing/success/page.tsx
@@ -32,13 +32,15 @@ export default async function Page() {
         });
 
         return (
-            <div className="flex flex-col items-center justify-center min-h-screen text-center">
-                <h1 className="text-2xl font-bold">Thanh Toán Thành Công</h1>
-                <p className="text-lg mt-2">Gói cước Premium đã đăng ký thành công!</p>
-                <Link href="/dashboard">
-                    <Button className="mt-4">Bắt Đầu Trải Nghiệm Premium</Button>
-                </Link>
-            </div>
+            <main className="mx-auto max-w-7x1 space-y-6 px-3 py-6 text-center">
+                <h1 className="text-3xl font-bold text-cyan-600">Thanh Toán Thành Công</h1>
+                <p>
+                    Gói cước Premium đã đăng kí thành công !!!
+                </p>
+                <Button asChild className="text-yellow-500">
+                    <Link href="/resumes">Bắt Đầu Trải Nghiệm Premium</Link>
+                </Button>
+            </main>
         );
     } catch (error) {
         console.error("Lỗi khi cập nhật trạng thái:", error);
